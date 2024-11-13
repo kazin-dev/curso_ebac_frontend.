0ca5cd06ab7a1e4c3334e54c55952244ded759a1
kauã <124106547+kazin-dev@users.noreply.github.com>
2024-11-13 17:20:47 -0300

    arrumando erros de script

--- JS/index.js
+++ JS/index.js
@@ -1,12 +1,13 @@
 const form = document.getElementById("Formulario");
-form.addEventListener("submit", function() {
-const valorA = document.getElementById("campoA").value;
-const valorB = document.getElementById("campoB").value;
-if (valorA < valorB) {
-    alert("tudo certo");
-}else{
-    alert("Tente novamente");
-}
-})
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    const valorA = document.getElementById("campoA").value;
+    const valorB = document.getElementById("campoB").value;
 
-
+    if (parseFloat(valorA) < parseFloat(valorB)) {
+        alert("Tudo certo!");
+    } else {
+        alert("Tente novamente");
+    }
+});
